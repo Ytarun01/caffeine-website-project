4dfc1667775c0df6c111dba41a912e60fb8702a8
TARUN YADAV <135727655+Ytarun01@users.noreply.github.com>
2023-08-04 11:18:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+var crsr = document.querySelector("#cursor");
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + 30 + "px";
+  crsr.style.top = dets.y + "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 2;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95c11e";
+    crsr.style.backgroundColor = "#9f7950";
+  });
+});
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1.5,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#aboutus img ,#aboutusin", {
+  y:90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#aboutus",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 50%",
+    scrub: 3,
+  },
+});
+gsap.from(".card", {
+  scale:0.8,
+ 
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 3,
+  },
+});
